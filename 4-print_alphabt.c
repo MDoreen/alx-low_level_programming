fc61b7e92b00e47c420404634374f5bc10069934
MDoreen <dmachoni@gmail.com>
2022-03-14 16:29:23 -0700

    4-print_alphabt.c

---
+++ 4-print_alphabt.c
@@ -0,0 +1,49 @@
+#include <stdio.h>
+
+int main(void)
+{
+    char ch = 'a';
+	while(ch <= 'z')
+	{
+		if (ch != 'e' && ch != 'q')
+		{
+			putchar(ch);
+		}
+		ch++;
+	}
+	putchar('\n');
+
+    return 0;
+}
+
+#include <stdio.h>
+
+int main(void)
+{
+    
+	for (char ch = 'a'; ch <= 'z'; ch++)
+	{
+		if (ch != 'e' && ch != 'q')
+		{
+			putchar(ch);
+		}
+	}
+	putchar('\n');
+
+    return 0;
+}
+
+int main(void)
+{
+    
+	for (char ch ='a'; ch <= 'z'; ch++)
+	{
+		if (ch == 'e' || ch == 'q')
+		    ch++;
+		putchar(ch);
+		
+	}
+	putchar('\n');
+
+    return 0;
+}
