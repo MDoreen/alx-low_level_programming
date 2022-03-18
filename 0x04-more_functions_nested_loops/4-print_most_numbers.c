105a8a65d6fe4da0288b39001f44eb487d9327ed
MDoreen <99642259+MDoreen@users.noreply.github.com>
2022-03-18 16:04:36 +0300

    Update 4-print_most_numbers.c

--- 0x04-more_functions_nested_loops/4-print_most_numbers.c
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -9,8 +9,8 @@ void print_most_numbers(void)
 
 	for (num = 0; num <= 9; num++)
 	{
-		if (num != 2 && num != 4)
-			_putchar((num % 10) + '0');
+	if (num != 2 && num != 4)
+	_putchar((num % 10) + '0');
 	}
 
 	_putchar('\n');
