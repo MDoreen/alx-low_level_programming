c519f005ab8ab01f11264b075f42a61a4a743156
MDoreen <dmachoni@gmail.com>
2022-04-11 12:16:17 -0700

    A unction-like macro SUM(x, y) that computes the sum of the numbers x and y

--- 0x0D-preprocessor/4-sum.h
+++ 0x0D-preprocessor/4-sum.h
@@ -1,10 +1,10 @@
 #ifndef SUM_H
 #define SUM_H
 
-/**
- * File: $-sum.h
- * Desc: Header defining a function-like macro SUM(x, y)
- * that computes the sum of the numbers x and y.
+/*
+ * File: 4-sum.h 
+ * Desc: Header file defining a function-like macro SUM(x, y)
+ *       that computes the sum of the numbers x and y.
  */
 
 #define SUM(x, y) ((x) + (y))
