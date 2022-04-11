48ddef35c35d66c3990c7da47586a3c21a09af64
MDoreen <dmachoni@gmail.com>
2022-04-11 11:21:22 -0700

    A header file that defines a macro named PI as an abbreviation for the token 3.14159265359

--- 0x0D-preprocessor/0-object_like_macro.h
+++ 0x0D-preprocessor/0-object_like_macro.h
@@ -4,7 +4,7 @@
 /*
  * File: 0-object_like_macro.h
  * Desc: Header file that defines a macro named
- *       SIZE as an abbreviation for the token 1024.
+ *       SIZE as an abbreviation for token 1024.
  */
 
 #define SIZE 1024
