388c71834ed1f58154f6846b8311e3fb5d315944
MDoreen <dmachoni@gmail.com>
2022-04-14 17:25:19 -0700

    A unction that returns the sum of all its parameters

--- 0x10-variadic_functions/0-sum_them_all.c
+++ 0x10-variadic_functions/0-sum_them_all.c
@@ -1,13 +1,25 @@
-#include "function_pointers.h"
-#include <stdlib.h>
+#include "variadic_functions.h"
+#include <stdarg.h>
+
 /**
- * print_name - prints the name
- * @name: name
- * @f: function
+ * sum_them_all - Returns the sum of all its paramters.
+ * @n: The number of paramters passed to the function.
+ * @...: A variable number of paramters to calculate the sum of.
+ *
+ * Return: If n == 0 - 0.
+ *         Otherwise - the sum of all parameters.
  */
-void print_name(char *name, void (*f)(char *))
+int sum_them_all(const unsigned int n, ...)
 {
-	if (name == NULL || f == NULL)
-		return;
-	f(name);
+	va_list nums;
+	unsigned int index, sum = 0;
+
+	va_start(nums, n);
+
+	for (index = 0; index < n; index++)
+		sum += va_arg(nums, int);
+
+	va_end(nums);
+
+	return (sum);
 }
