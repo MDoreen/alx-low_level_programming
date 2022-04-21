3d1e0b52bc9a74b811c0ef3719b0896b79a3e257
MDoreen <dmachoni@gmail.com>
2022-04-21 15:17:27 -0700

    Add node

--- 0x12-singly_linked_lists/2-add_node.c
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -7,7 +7,7 @@
  * @head: A pointer to the head of the list_t list.
  * @str: The string to be added to the list_t list.
  *
- * Return: If the function fails - NULL.
+ * Return: If the function fails - Null
  *         Otherwise - the address of the new element.
  */
 list_t *add_node(list_t **head, const char *str)
