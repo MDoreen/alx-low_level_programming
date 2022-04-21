35ba3c21b2ca7d681292eaca80eb605df0236f06
MDoreen <dmachoni@gmail.com>
2022-04-21 14:31:08 -0700

    linked list

--- 0x10-variadic_functions/0-sum_them_all.c
+++ 0x10-variadic_functions/0-sum_them_all.c
@@ -1,5 +1,5 @@
 #include "variadic_functions.h"
-#include <stdarg.h>
+#include <stdio.h>
 
 /**
  * sum_them_all - Returns the sum of all its paramters.
---
+++ 0x12-singly_linked_lists/README.md
@@ -0,0 +1,1 @@
+This is a file for singly linked lists project in C.
